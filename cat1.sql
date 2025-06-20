0c99792c0a940021ec41bc6bb8812002bf756ad5
END-119 <hei.endrika.3@gmail.com>
2025-06-20 11:33:55 +0300

    Add files via upload

---
+++ cat1.sql
@@ -0,0 +1,9 @@
+ 1 - c'est une table mais qui ne stocke pas de donné (pseudo-table)
+
+2 - Create sequence
+
+3 - a relier deux table 
+
+4- insert into "user" value ('username', 'endrika@gmail.com') 
+
+5- INSERT INTO film VALUES ('le prisonnier d'Azkaban')
